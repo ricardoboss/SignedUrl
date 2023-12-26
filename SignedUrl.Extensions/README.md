63da89b863de5ea873bef7ab67e08769b9a3d53a
Ricardo Boss <mail@ricardoboss.de>
2023-12-26 12:47:00 +0100

    Added SignedUrl.Extensions and fixed link to license in READMEs

--- SignedUrl.Extensions/README.md
+++ SignedUrl.Extensions/README.md
@@ -1,6 +1,6 @@
-# SignedUrl.Abstractions
+# SignedUrl.Extensions
 
-[![NuGet](https://img.shields.io/nuget/v/SignedUrl.AspNet.svg)](https://www.nuget.org/packages/SignedUrl.AspNet/)
+[![NuGet](https://img.shields.io/nuget/v/SignedUrl.Extensions.svg)](https://www.nuget.org/packages/SignedUrl.Extensions/)
 
 See [SignedUrl](https://www.nuget.org/packages/SignedUrl/) for usage.
 
---
+++ SignedUrl.Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,40 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using SignedUrl.Abstractions;
+using SignedUrl.AspNet;
+
+namespace SignedUrl.Extensions;
+
+/// <summary>
+/// Extension methods for <see cref="IServiceCollection" />.
+/// </summary>
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// <para>
+    /// Adds signed URL services to the specified <see cref="IServiceCollection" />.
+    /// </para>
+    /// <list type="bullet">
+    /// <item><description><see cref="ISignatureProtector" /> is registered as a singleton and uses <see cref="DataProtectionSignatureProtector" />.</description></item>
+    /// <item><description><see cref="ISignatureGenerator" /> is registered as a singleton and uses <see cref="DigestSignatureGenerator" />.</description></item>
+    /// <item><description><see cref="IUrlSigner" /> is registered as a singleton and uses <see cref="QueryStringUrlSigner" />.</description></item>
+    /// </list>
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection" /> to add services to.</param>
+    /// <param name="queryKeyProvider">An optional function that returns the query key to use for the signature.</param>
+    /// <returns>The <see cref="IServiceCollection" /> so that additional calls can be chained.</returns>
+    public static IServiceCollection AddSignedUrl(this IServiceCollection services, Func<string>? queryKeyProvider = null)
+    {
+        services.TryAddSingleton<ISignatureProtector, DataProtectionSignatureProtector>();
+        services.TryAddSingleton<ISignatureGenerator, DigestSignatureGenerator>();
+
+        services.AddSingleton<IUrlSigner, QueryStringUrlSigner>(sp =>
+        {
+            var generator = sp.GetRequiredService<ISignatureGenerator>();
+
+            return new(generator, queryKeyProvider?.Invoke() ?? "s");
+        });
+
+        return services;
+    }
+}
